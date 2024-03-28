27fc5088d9fc97c9e46e9d4182589c5e3b747a0b
OjModupe <134464057+OjModupe@users.noreply.github.com>
2024-03-28 13:51:14 +0000

    2-binary_tree_insert_right.c

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,25 +1,30 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_right -
- *
- * Return: whatever 
+ * binary_tree_insert_right - inserts a node as the right-child
+ * of another node
+ * @parent: pointer to the node to insert the right-child in
+ * @value: value to store in the new node
+ * Return: new node or NULL if fail
  */
+
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-binary_tree_t *node_right;
+	binary_tree_t *store = NULL;/* will store the address of parent left-child */
 
-if (parent == NULL)
-return (NULL);
+	if (parent == NULL)
+		return (NULL);
 
-node_right = binary_tree_node(parent, value);
-if (node_right == NULL)
-return (NULL);
-if (parent->right)
-{
-parent->right->parent = node_right;
-node_right->right = parent->right;
-}
-parent->right = node_right;
-return (node_right);
+	if (parent->right != NULL)
+	{
+		store = parent->right;
+		parent->right = binary_tree_node(parent, value);
+		parent->right->right = store;
+		store->parent = parent->right;
+		store = NULL;
+		return (parent->right);
+	}
+
+	parent->right = binary_tree_node(parent, value);
+	return (parent->right);
 }
