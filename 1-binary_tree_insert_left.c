c7bfa0a16155d4c5f235ce8b9b9cf6be18406397
OjModupe <134464057+OjModupe@users.noreply.github.com>
2024-03-28 13:03:57 +0000

    0-binary_tree_node.c

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,43 +1,18 @@
-#include "binary_trees_h"
-
+#include "binary_trees.h"
 /**
  * binary_tree_node - create a node
- *
  * @parent: pointer to parent
- * @value: value
- *
- * Return: pointer to the created node
+ * Return: pointer to the new node
  */
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *nodo;
+binary_tree_t *nodo;
 
-	if (parent == NULL)
-		return (NULL);
-
-	nodo = calloc(1, sizeof(binary_tree_t));
-	if (nodo == NULL)
-		return (NULL);
-	if (parent->left == NULL)
-	{
-		parent->left = nodo
-		nodo->parent = parent;
-		nodo->n = value;
-		nodo->right = NULL;
-		nodo->left = NULL;
-	}
-	else
-	{
-		nodo->left = parent->left;
-		parent->left->parent = nodo;
-		parent->left = nodo;
-		nodo->n = value;
-		nodo->parent = parent;
-		nodo->right = NULL;
-	}
-	return (nodo);
-}
-
-
-
-			
+nodo = malloc(sizeof(binary_tree_t));
+if (nodo == NULL)
+return (NULL);
+nodo->n = value;
+nodo->parent = parent;
+nodo->left = NULL;
+nodo->right = NULL;
+return			
